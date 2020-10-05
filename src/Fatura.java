b7fa194f40d417c7322c9de60b0bb508af79419a
Leticia Mello <leticiamellocunha9@yahoo.com.br>
2020-10-05 08:01:39 -0300

    Classe Fatura

---
+++ src/Fatura.java
@@ -0,0 +1,33 @@
+
+public class Fatura {
+	
+	String cliente;
+	String data;
+	double valorTotal;
+	
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+	
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public double getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+
+}
