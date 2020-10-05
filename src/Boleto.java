71105710d018b15c48122c59f78d967236a58cc8
Leticia Mello <leticiamellocunha9@yahoo.com.br>
2020-10-05 08:01:24 -0300

    Classe Boleto

---
+++ src/Boleto.java
@@ -0,0 +1,34 @@
+
+public class Boleto {
+
+	double valorPago;
+	String data;
+	String codigo;
+	
+	public double getValorPago() {
+		return valorPago;
+	}
+
+	public void setValorPago(double valorPago) {
+		this.valorPago = valorPago;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+}
